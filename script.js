868b6ffa404eb51c00cad68029345f1d119800e1
Adam <adam.conkle@gmail.com>
2025-03-12 15:02:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-/*let qrcode = new QRCode(document.getElementById("qrcode"));
-
-function makeCode () {		
-	let elText = document.getElementById("text");
-	
-	if (!elText.value) {
-		alert("Input a text");
-		elText.focus();
-		return;
-	}
-	
-	qrcode.makeCode(elText.value);
-}
-
-makeCode();
-
-$("#text").
-	on("blur", function () {
-		makeCode();
-	}).
-	on("keydown", function (e) {
-		if (e.keyCode == 13) {
-			makeCode();
-		}
-	});
-*/
-
 let qrcode = new QRCode(document.getElementById("qrcode"));
 
 function makeCode() {		
